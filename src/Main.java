218392acd010255ea970c730c4489165631251bc
Higor Vinicius <carloshigor409@gmail.com>
2025-12-04 13:05:56 -0300

    Remove tips and add variable examples in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,83 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //para numeros inteiros//
+        int minhaIdade = 22;
+
+        //para escritas//
+        String meuNome = "Higor Vinicius";
+
+        //para nunca precisar declarar variavel//
+        var MeuVeiculo = "Ford mustang";
+
+        //para numeros não inteiros//
+        double meuNumero = 10.0;
+
+        //para apenas uma letra escrita//
+        char primeiro = 'A';
+
+        //para verdadeiro ou falso//
+        boolean bool = true;
+
+
+        //conciocionais para determinar uma ação//
+        if(primeiro == 'b') {
+            System.out.println("Verdadeiro");
         }
+        else {
+            System.out.println("Falso");
+        }
+
+        //Vetores//
+        int[] colecaoDeInteiros = {1 ,2 ,3 ,4 ,5};
+        System.out.println(colecaoDeInteiros[0]);
+        boolean[] colecaoDeBoolean = {true , false , true , false};
+        System.out.println(colecaoDeBoolean[3]);
+
+        //ARR//
+        String[] nomesArr = new String[10];
+        nomesArr[0] = "Hugo";
+        nomesArr[1] = "Diana";
+        nomesArr[2] = "Bruna";
+        nomesArr[3] = "Gilvan";
+        nomesArr[4] = "Daniel";
+
+        //arraylists//
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Fernanda");
+        nomes.add("Higor");
+        nomes.add("Renata");
+        nomes.add("Danielle");
+
+
+        //loops//
+        for (int meuInterador = 0; meuInterador < nomes.size(); meuInterador++){
+            System.out.println(nomes.get(meuInterador));
+        }
+
+        int contador = 0;
+        while(contador < 10){
+            System.out.println("Estou no while");
+            contador++;
+        }
+
+        //Casting = mudar o valor de uma variavel//
+        double resultado = 0.0;
+        int resultadoInt = (int) resultado;
+
+        //casting implicito
+        int meuInt = 10;
+        double meuDouble = meuInt;
+
+        //casting explicito//
+        String meuString = "10";
+        int meuInt2 = Integer.parseInt(meuString);
+        //ou//
+        String minhaString = String.valueOf(meuInt2);
+
+
+
     }
 }
+
